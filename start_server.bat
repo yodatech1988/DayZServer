288a566b5c93daed18abd9ade1be9fdae2bc9c07
yodatech1988 <123972919+yodatech1988@users.noreply.github.com>
2025-12-01 13:42:36 -0500

    Add files via upload

---
+++ start_server.bat
@@ -0,0 +1,7 @@
+cd /d "C:\dayzserver"
+DayZServer_x64.exe ^
+  "-config=serverDZ.cfg" ^
+  "-ip=10.8.0.2" ^
+  "-port=2302" ^
+  "-profiles=C:\dayzserver\profiles" ^
+  "-dologs" -adminlog -netlog -freezecheck
